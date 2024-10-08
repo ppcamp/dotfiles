9754429ec04cb8bfb8823c50e774c6542288b439
Pedro Augusto Campos Dos Santos <38117637+ppcamp@users.noreply.github.com>
2024-10-08 16:28:41 -0300

    fix: script module loading

--- lua/config/scripts.lua
+++ lua/config/scripts.lua
@@ -1,1 +1,14 @@
-vim.cmd('runtime! lua/scripts/**/*.lua')
+-- vim.cmd('runtime! lua/scripts/**/*.lua')
+
+local function load_lua_files_from_dir(dir)
+    local files = vim.fn.readdir(dir)
+    for _, file in ipairs(files) do
+        if file:match('%.lua$') then -- match lua files only
+            local fpath = 'scripts.' .. file:gsub('%.lua$', '')
+            require(fpath)
+        end
+    end
+end
+
+local dir = vim.fn.stdpath('config') .. '/lua/scripts'
+load_lua_files_from_dir(dir)
