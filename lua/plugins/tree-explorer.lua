24e586adb468280ac4dfd68c320e56b41bc3a592
Pedro Augusto Campos Dos Santos <38117637+ppcamp@users.noreply.github.com>
2024-10-11 15:31:31 -0300

    feat: add tree view to nvim

---
+++ lua/plugins/tree-explorer.lua
@@ -0,0 +1,12 @@
+-- Highlight todo, notes, etc in comments
+
+return {
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = { on_attach = on_attach_change }
+
+        -- open it by typing :NvimTreeToggle
+        -- and then, you can type "a"
+    }
+}
+-- vim: ts=2 sts=2 sw=2 et
