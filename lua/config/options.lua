7d1e8323bfa54580f4aeda5c55e80ceecf4652db
Pedro Santos <p.augustocampos@gmail.com>
2025-03-03 20:39:17 -0300

    feat: config to use relative numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,19 +6,19 @@ vim.g.have_nerd_font = true
 
 opt.cursorline = true -- highlight current line
 
-opt.number = true     -- Set line numbering
+opt.number = true -- Set line numbering
 opt.ignorecase = true -- Ignore case when searching
 
 -- Set indentation options
-opt.shiftwidth = 4   -- Number of spaces for each indentation level
-opt.tabstop = 4      -- Number of spaces a tab character represents
+opt.shiftwidth = 4 -- Number of spaces for each indentation level
+opt.tabstop = 4 -- Number of spaces a tab character represents
 opt.expandtab = true -- Use spaces instead of tabs
-
 
 -- Use ripgrep (rg) for :grep
 opt.grepprg = "rg --vimgrep --no-heading --smart-case"
 -- Set the format for parsing grep output
 opt.grepformat = "%f:%l:%c:%m"
 
+opt.relativenumber = true
 
 -- vim: ts=2 sts=2 sw=2 et
