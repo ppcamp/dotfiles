51c2c38be5c29f53363cbcc4ef8be3f66c53dd02
Pedro Santos <p.augustocampos@gmail.com>
2025-03-08 22:05:09 -0300

    feat: add copilot shortcut

--- lua/plugins/copilot-ai.lua
+++ lua/plugins/copilot-ai.lua
@@ -3,6 +3,12 @@
 -- To setup local, run :Copilot setup
 return {
 	"github/copilot.vim",
+	config = function()
+		vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
+	end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
