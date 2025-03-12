d00546499fc7a2e99e3e2d1985b128d8ee0c1358
Pedro Santos <p.augustocampos@gmail.com>
2025-03-12 20:14:31 -0300

    feat: add and document TODO list

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,12 +1,57 @@
--- Highlight todo, notes, etc in comments
---
+-- @p
+-- BUG:
+-- FIX:
+-- FIXME:
+-- HACK:
+-- INFO:
+-- NOTE:
+-- OPTIM:
+-- OPTIMIZE:
+-- PERF:
+-- PERFORMANCE:
+-- TESTING:
+-- FAILED:
+-- PASSED:
+-- TODO:
+-- WARN:
+-- WARNING:
+-- XXX:
+-- IDEA:
+-- CHECK:
+-- REVIEW:
+-- ASK:
+-- QUESTION:
 
+-- Highlight todo, notes, etc in comments
 return {
 	{
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
+		opts = { signs = true },
+		config = function()
+			vim.keymap.set(
+				"n",
+				"<leader>se",
+				"<cmd>TodoTelescope<cr>",
+				{ desc = "TODO: search", noremap = true, silent = true }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>td",
+				"<cmd>TodoQuickFix<CR>",
+				{ desc = "TODO: Quickfix panel", noremap = true, silent = true }
+			)
+
+			local todo = require("todo-comments")
+			todo.setup({
+				keywords = {
+					REVIEW = { icon = "", color = "hint", alt = { "CHECK" } },
+					IDEA = { icon = "", color = "info", alt = { "THEORY" } },
+					QUESTION = { icon = "󱜺", color = "warning", alt = { "ASK" } },
+				},
+			})
+		end,
 	},
 }
 
