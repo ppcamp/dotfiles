6851d9450b60aa59c30ccf10083c3c541246e254
Pedro Santos <p.augustocampos@gmail.com>
2025-03-12 17:56:37 -0300

    feat: add nvim cheatsheet helper

---
+++ lua/plugins/nvim-helper.lua
@@ -0,0 +1,20 @@
+-- It shows nvim cheatsheet
+
+return {
+	"mrjones2014/legendary.nvim",
+	-- since legendary.nvim handles all your keymaps/commands,
+	-- its recommended to load legendary.nvim before other plugins
+	priority = 10000,
+	lazy = false,
+	-- sqlite is only needed if you want to use frecency sorting
+	-- dependencies = { 'kkharji/sqlite.lua' },
+	config = function()
+		vim.keymap.set(
+			"n",
+			"<leader>?v",
+			"<cmd>Legendary<cr>",
+			{ desc = "Legendary: Nvim cheatsheet", noremap = true, silent = true }
+		)
+	end,
+}
+-- vim: ts=2 sts=2 sw=2 et
