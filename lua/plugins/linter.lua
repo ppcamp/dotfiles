8b9a757a3c78fdc487df303bf467368f63a929f4
Pedro Santos <p.augustocampos@gmail.com>
2025-03-18 19:06:15 -0300

    fix: disable linting

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -5,15 +5,12 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			local lint = require("lint")
-			lint.linters_by_ft = lint.linters_by_ft or {}
+			-- lint.linters_by_ft = lint.linters_by_ft or {}
+
+			-- if we don't specify to not use linters, it'll throw error everytime
+			lint.linters_by_ft = {}
 
 			-- lint.linters_by_ft["markdown"] = { "markdownlint" } -- NOTE: it requires markdown/nodejs
-
-			-- To allow other plugins to add linters to require('lint').linters_by_ft,
-			-- instead set linters_by_ft like this:
-			-- lint.linters_by_ft = lint.linters_by_ft or {}
-			-- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-			--
 			-- However, note that this will enable a set of default linters,
 			-- which will cause errors unless these tools are available:
 			-- {
@@ -28,18 +25,6 @@ return {
 			--   terraform = { "tflint" },
 			--   text = { "vale" }
 			-- }
-			--
-			-- You can disable the default linters by setting their filetypes to nil:
-			-- lint.linters_by_ft['clojure'] = nil
-			-- lint.linters_by_ft['dockerfile'] = nil
-			-- lint.linters_by_ft['inko'] = nil
-			-- lint.linters_by_ft['janet'] = nil
-			-- lint.linters_by_ft['json'] = nil
-			-- lint.linters_by_ft['markdown'] = nil
-			-- lint.linters_by_ft['rst'] = nil
-			-- lint.linters_by_ft['ruby'] = nil
-			-- lint.linters_by_ft['terraform'] = nil
-			-- lint.linters_by_ft['text'] = nil
 
 			-- Create autocommand which carries out the actual linting
 			-- on the specified events.
