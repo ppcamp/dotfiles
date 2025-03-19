3e78501fbc80172f5965590a13a165eaf3534364
Pedro Santos <p.augustocampos@gmail.com>
2025-03-19 12:17:40 -0300

    feat: add git line blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,19 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup()
+		require("gitsigns").setup({
+			current_line_blame = true,
+
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 500,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
+
+			current_line_blame_formatter = "[<author>, <author_time:%R>] <summary>",
+		})
 	end,
 }
