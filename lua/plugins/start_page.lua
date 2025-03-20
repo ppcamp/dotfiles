9a115146f87b3768b2c81f2a67d5e87db699b612
Pedro Santos <p.augustocampos@gmail.com>
2025-03-20 19:24:36 -0300

    feat: add startpage

---
+++ lua/plugins/start_page.lua
@@ -0,0 +1,10 @@
+return {
+	"goolord/alpha-nvim",
+	config = function()
+		local theme = require("alpha.themes.startify").config
+
+		require("alpha").setup(theme)
+	end,
+}
+
+-- vim: ts=2 sts=2 sw=2 et
