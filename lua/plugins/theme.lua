26af6abfa362604242e50e35ca8ebd8e34520175
Pedro Santos <p.augustocampos@gmail.com>
2025-03-24 19:27:36 -0300

    feat: theme change

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,18 +1,35 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"rebelot/kanagawa.nvim",
+	-- "marko-cerovac/material.nvim",
+	-- "tiagovla/tokyodark.nvim",
 
-	-- "polirritmico/monokai-nightasty.nvim",
-	-- "folke/tokyonight.nvim",
-	-- "rose-pine/neovim",
+	-- "catppuccin/nvim",
 	-- "Mofiqul/dracula.nvim",
+	-- "rose-pine/neovim",
 
 	priority = 1000,
 	config = function()
-		-- vim.cmd("colorscheme dracula")
-		-- vim.cmd("colorscheme dracula-soft")
-		-- vim.cmd.colorscheme("tokyonight-storm")
-		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.cmd.colorscheme("kanagawa")
+		-- NOTE: default that doesn't need to install extra
+		-- habamax
+		-- sorbet
+		-- slat
+		-- lunaperche
+		-- retrobox
+		-- desert
+		-- minischeme
+		-- wildcharm
+		-- wildcharm
+		-- vim
+
+		-- vim.cmd.colorscheme("material-darker")
+		-- vim.cmd.colorscheme("material-palenight")
+		-- vim.cmd.colorscheme("tokyodark")
+
+		-- vim.cmd.colorscheme("rose-pine-moon")
+		-- vim.cmd.colorscheme("dracula")
+		-- vim.cmd.colorscheme("dracula-soft")
+		-- vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
 
@@ -24,23 +41,6 @@ return {
 --         -- This defaults to nil and will use the default ansi colors for the theme
 --         ansi_colors_name = nil,
 --     },
--- }
-
--- return {
---   { -- You can easily change to a different colorscheme.
---     -- Change the name of the colorscheme plugin below, and then
---     -- change the command in the config to whatever the name of that colorscheme is.
---     --
---     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
---     'folke/tokyonight.nvim',
---     priority = 1000, -- Make sure to load this before all the other start plugins.
---     init = function()
---       -- Load the colorscheme here.
---       -- Like many other themes, this one has different styles, and you could load
---       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
---       vim.cmd.colorscheme 'tokyonight-night'
---     end,
---   },
 -- }
 
 -- vim: ts=2 sts=2 sw=2 et
