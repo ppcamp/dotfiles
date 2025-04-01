b7362e9db1804267bdba22e4ebbddda7dbffb2c7
Pedro Santos <p.augustocampos@gmail.com>
2025-04-01 15:16:42 -0300

    fix: git diff shortcut

--- lua/plugins/git-diff-view.lua
+++ lua/plugins/git-diff-view.lua
@@ -1,30 +1,29 @@
 return {
-	'sindrets/diffview.nvim',
+	"sindrets/diffview.nvim",
 	priority = 10,
 	config = function()
-		require('diffview').setup()
+		require("diffview").setup()
 
 		local map = function(lhs, rhs, mode, opts)
 			opts = opts or {}
-			mode = mode or 'n'
+			mode = mode or "n"
 			opts.silent = true
 			opts.noremap = true
 			vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 		end
 
-		map('<leader>gd', '<cmd>DiffviewOpen<CR>')
-		map('<leader>gD', '<cmd>DiffviewClose<CR>')
-		map('<leader>gr', '<cmd>DiffviewRefresh<CR>')
-		map('<leader>gv', '<cmd>DiffviewToggleFiles<CR>')
-		map('<leader>gp', '<cmd>DiffviewFileHistory %<CR>')
-
+		map("<leader>gd", "<cmd>DiffviewOpen<CR>")
+		map("<leader>gD", "<cmd>DiffviewClose<CR>")
+		map("<leader>gr", "<cmd>DiffviewRefresh<CR>")
+		map("<leader>gv", "<cmd>DiffviewToggleFiles<CR>")
+		map("<leader>gp", "<cmd>DiffviewFileHistory %<CR>")
 
 		require("which-key").add({
-			{ '<leader>gd', 'DiffView: Open' },
-			{ '<leader>gD', 'DiffView: Close' },
-			{ '<leader>gr', 'DiffView: Refresh' },
-			{ '<leader>gv', 'DiffView: Toggle Files' },
-			{ '<leader>gp', 'DiffView: File History' },
+			{ "<leader>gd", desc = "DiffView: Open" },
+			{ "<leader>gD", desc = "DiffView: Close" },
+			{ "<leader>gr", desc = "DiffView: Refresh" },
+			{ "<leader>gv", desc = "DiffView: Toggle Files" },
+			{ "<leader>gp", desc = "DiffView: File History" },
 		})
-	end
+	end,
 }
