0251db659d81337f76b640952fb819b48509daaf
Pedro Campos <p.augustocampos@gmail.com>
2025-05-29 18:05:08 -0300

    fix: pet not executing, neither adding to buffer

--- dot_config/shell/internal/plugins/pet/shortcuts.zsh
+++ dot_config/shell/internal/plugins/pet/shortcuts.zsh
@@ -7,11 +7,13 @@ export FZF_CTRL_R_OPTS="
 
 ############################# Search in your pet config bookmarks
 pet-view() {
-    local query=$LBUFFER                # keep previous cmdline value
-    zle -I                              # Clears any pending input/output
-    BUFFER=""                           # Clears the line buffer
-    zle accept-line                     # Refresh/remove line buffer (previous data)
-    pet search --color --query="$query" # run query with previous cmd value
+  local query=$LBUFFER              # keep previous cmdline value
+  zle -I                            # Clears any pending input/output
+  BUFFER=""                         # Clears the line buffer
+  zle accept-line                   # Refresh/remove line buffer (previous data)
+  pet exec --color --query="$query" # run query with previous cmd value
+  # pet cannot run in a subshell, aka $(), otherwise it won't identify as TTY
+  # and thus, won't put color in the output.
 }
 zle -N pet-view
 bindkey '^s' pet-view
